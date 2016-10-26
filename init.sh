e6626cbec2b2acc6aed3bcb8d90b2a87d1dab47c
Christian Fischer <christian@chfi.se>
2016-10-26 12:51:34 +0200

    Add spacemacs, zshrc, key layout

---
+++ init.sh
@@ -0,0 +1,4 @@
+DOTPATH="$HOME/.dotfiles"
+
+ln -sv "$DOTPATH/.spacemacs" "$HOME/.spacemacs"
+ln -sv "$DOTPATH/.zshrc" "$HOME/.zshrc"
---
+++ setkeyboard.sh
@@ -0,0 +1,1 @@
+setxkbmap -I ~/.xkb svorakcode -variant svorakcode -print | xkbcomp -I/home/christian/.xkb - :0.0

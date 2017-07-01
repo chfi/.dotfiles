03f9d25f521627618178b1e1ce18141b3af8183a
Christian Fischer <christian@chfi.se>
2017-07-01 14:59:35 +0200

    update setkeyboard script to silence warnings

--- setkeyboard.sh
+++ setkeyboard.sh
@@ -1,1 +1,1 @@
-setxkbmap -I ~/.xkb svorakcode -variant svorakcode -print | xkbcomp -I/home/christian/.xkb - :0.0
+setxkbmap -I ~/.xkb svorakcode -variant svorakcode -print | xkbcomp -w0 -I/home/christian/.xkb - :0.0
